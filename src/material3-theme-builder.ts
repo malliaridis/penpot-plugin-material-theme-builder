9b2bc34096a62d359a42e3c82ec069ec82a36d00
Christos Malliaridis <c.malliaridis@gmail.com>
2024-12-19 18:49:42 +0100

    Make tonal palette generation optional and disable by default

--- src/material3-theme-builder.ts
+++ src/material3-theme-builder.ts
@@ -7,7 +7,12 @@ import {
 } from "@material/material-color-utilities";
 import {JsonScheme, Palettes, Schemes, stateOpacities, toneValues} from "./model.ts";
 
-export function createThemeColors(themeName: string, sourceColor: string) {
+export function createThemeColors(
+    themeName: string,
+    sourceColor: string,
+    withTonalPalettes: Boolean = false,
+    withStateLayers: Boolean = true,
+) {
   const theme: Theme = themeFromSourceColor(argbFromHex(sourceColor));
 
   // Create source color
@@ -21,15 +26,11 @@ export function createThemeColors(themeName: string, sourceColor: string) {
   for (const scheme in theme.schemes) {
     const jsonScheme: JsonScheme =
       theme.schemes[scheme as keyof Schemes].toJSON();
-    createColorScheme(themeName, scheme, jsonScheme);
+    createColorScheme(themeName, scheme, jsonScheme, withStateLayers);
   }
 
   // Create palette color entries
-  for (const palette in theme.palettes) {
-    const tonalPalette: TonalPalette =
-      theme.palettes[palette as keyof Palettes];
-    createColorPalette(themeName, palette, tonalPalette);
-  }
+  if (withTonalPalettes) createTonalPalettes(themeName, theme.palettes);
 
   // TODO Add support for theme.customColors
 }
@@ -51,20 +52,6 @@ function createColorScheme(
   }
 }
 
-function createColorPalette(
-  themeName: string,
-  name: string,
-  palette: TonalPalette,
-) {
-  toneValues.forEach((tone) => {
-    const argbColor = palette.tone(tone);
-    const color = penpot.library.local.createColor();
-    color.color = hexFromArgb(argbColor);
-    color.path = `${themeName}/palettes`;
-    color.name = `${name}-${tone.toString()}`;
-  });
-}
-
 function createStateLayerColors(
   themeName: string,
   name: string,
@@ -77,5 +64,26 @@ function createStateLayerColors(
     stateColor.opacity = opacity;
     stateColor.path = `${themeName}/state-layers/${name}/${colorName}`;
     stateColor.name = `opacity-${opacity.toFixed(2).toString()}`
+  });
+}
+
+function createTonalPalettes(themeName: string, palettes: Palettes) {
+  for (const palette in palettes) {
+    const tonalPalette: TonalPalette = palettes[palette as keyof Palettes];
+    createColorPalette(themeName, palette, tonalPalette);
+  }
+}
+
+function createColorPalette(
+    themeName: string,
+    name: string,
+    palette: TonalPalette,
+) {
+  toneValues.forEach((tone) => {
+    const argbColor = palette.tone(tone);
+    const color = penpot.library.local.createColor();
+    color.color = hexFromArgb(argbColor);
+    color.path = `${themeName}/palettes`;
+    color.name = `${name}-${tone.toString()}`;
   });
 }
